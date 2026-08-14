fbf5a606415f01905761f7e2ecbb5e1be65bfdee
Yahya Ouamar <yahyaouamar@proton.me>
2026-08-14 09:13:18 +0200

    finished adding housing location info interface

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {HousingLocation} from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housing-location/housing-location';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,16 @@ import {HousingLocation} from '../housing-location/housing-location';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
