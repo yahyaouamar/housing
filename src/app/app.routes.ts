15fae1c9b18a7963f73b85796d7a26b652f8a714
Yahya Ouamar <yahyaouamar@proton.me>
2026-08-20 18:17:19 +0200

    define a route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'App Home Page',
+    component: Home,
+  },
+];
