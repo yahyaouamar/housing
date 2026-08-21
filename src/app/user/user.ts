1c0bd939bbdb0c3bbcdb7465aa43cb4ac766b5fb
Yahya Ouamar <yahyaouamar@proton.me>
2026-08-21 17:46:40 +0200

    work in progress - reactive forms

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule,FormControl,FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
   username = 'youngTech';
   favoriteFramework = '';
+
   displayedFramework = '';
+
   showFramework() {
-  alert(this.favoriteFramework);
+    alert(this.favoriteFramework);
   }
+  handleSubmit() {
+  alert(this.profileForm.value.name + ' | ' + this.profileForm.value.email);
+  }
+  profileForm = new FormGroup({
+  name: new FormControl(''),
+  email: new FormControl(''),
+  });
 }
